bb854841f566febce960386110d0039d33abcf41
Zeeyon <zeeyon.c@gmail.com>
2020-06-25 21:07:25 +0900

    project init

---
+++ src/test/java/com/kakaopay/scattering/ScatteringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kakaopay.scattering;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScatteringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
