a1904a18f11e6fb9a189d254a79ed1adc2af4321
Zeeyon <zeeyon.c@gmail.com>
2020-07-05 11:58:35 +0900

    refactor: 이벤트 조회시 ScatteredMoney, ReceiveHistory 페치 조인

--- src/main/java/com/kakaopay/scattering/domain/ScatterEventRepo.java
+++ src/main/java/com/kakaopay/scattering/domain/ScatterEventRepo.java
@@ -1,9 +1,15 @@
 package com.kakaopay.scattering.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
 public interface ScatterEventRepo extends JpaRepository<ScatterEvent, Long> {
-    Optional<ScatterEvent> findByTokenAndRoomId(Token token, String roomId);
+    @Query("select e from ScatterEvent e " +
+            " left join fetch e.scatteredMonies.monies m " +
+            " left join fetch m.receiveHistory" +
+            " where e.token = :token and e.roomId = :roomId")
+    Optional<ScatterEvent> findByTokenAndRoomId(@Param("token") Token token, @Param("roomId") String roomId);
 }
